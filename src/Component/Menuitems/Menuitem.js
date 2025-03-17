7de9d3f1a5ec53c101d12f1e578dc7979e7aa661
Kavitarupali23 <kavita762k3@gmail.com>
2025-03-17 13:04:05 +0530

    hello

---
+++ src/Component/Menuitems/Menuitem.js
@@ -0,0 +1,41 @@
+const Menuitems = [
+    
+    {
+        icon: '/sidebar/dashboard.png',
+        label: "Dashboard",
+        href: "/",
+    },
+    {
+      icon: '/sidebar/project.png',
+      label: "Projects",
+      href: "/projects",
+    },
+    {
+       icon: "/sidebar/task.png",
+       label: "Task",
+       href: "task",
+      },
+    {
+      icon: "/sidebar/meeting schedule.png",
+      label: "Meeting Schedule",
+      href: "/meeting schedule",
+    },
+    {
+      icon: "/sidebar/client.png",
+      label: "Clients",
+      href: "/clients",
+    },
+    {
+      icon: "/sidebar/leads.png",
+      label: "Leads",
+      href: "/leads",
+    },
+    {
+      icon: "/sidebar/finance.png",
+      label: "Finance",
+      href: "/finance",
+    },
+
+];
+
+export default Menuitems;
